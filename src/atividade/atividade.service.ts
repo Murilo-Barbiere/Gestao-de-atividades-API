f1f032a676b62f484a29d728109b0380f3fbfafe
Murilo-Barbiere <murilobarbiere@gmail.com>
2026-07-08 09:41:14 -0400

    create_atividade_filho

--- src/atividade/atividade.service.ts
+++ src/atividade/atividade.service.ts
@@ -2,7 +2,7 @@ import { IAtividadeRepository } from './repository/iatividade.repository';
 import { AtividadeCreateDto } from './dto/atividade.create.dto';
 import { AtividadeResponseDto } from './dto/atividade.response.dto';
 import { ProjetoService } from '../projeto/projeto.service';
-import { Injectable, NotFoundException, UnauthorizedException} from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException, UnauthorizedException} from '@nestjs/common';
 import { AtividadeEntity } from './entity/atividade.entity';
 import { AtividadeUpdateDto } from './dto/atividade.update.dto';
 import { BuscarAtividadesQueryDto } from './dto/buscar_atividades_query.dto';
@@ -21,6 +21,13 @@ export class AtividadeService {
 
     async create(idUserAuth: number, atividadeCreateDto: AtividadeCreateDto): Promise<AtividadeResponseDto>{
         if(! await this.isAuthorized(atividadeCreateDto.idProjeto, idUserAuth)) throw new UnauthorizedException();
+
+        if(atividadeCreateDto.paiId){
+            const atividadePai = await this.atividadeRepository.findById(atividadeCreateDto.paiId)
+            if(atividadePai.projeto_id != atividadeCreateDto.idProjeto){
+                throw new ForbiddenException("Você não tem permissão para criar atividades neste projeto.");
+            }   
+        }
 
         return await this.atividadeRepository.create(atividadeCreateDto);
     }
@@ -47,7 +54,7 @@ export class AtividadeService {
             direcao: query.order ?? 'asc',
         }; 
         const atividades = await this.atividadeRepository.findByListaId(filtro);
-        const atividadeEntity = await atividades.map(atividade => this.toResponseDto(atividade));
+        const atividadeEntity = atividades.map(atividade => this.toResponseDto(atividade));
 
         return this.hierarquia(atividadeEntity);
     }
